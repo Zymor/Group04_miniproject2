328f242ae4c775ce586314552fc3c6b97d44c8b2
Starling <nakboyloy50@gmail.com>
2026-06-12 14:04:21 +0700

    update view

---
+++ Controller.java
@@ -0,0 +1,28 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private ChoiceBox<?> choicebox1;
+
+    @FXML
+    private ChoiceBox<?> choicebox2;
+
+    @FXML
+    private ChoiceBox<?> choicebox3;
+
+    @FXML
+    private Label label_result;
+
+    @FXML
+    private Label timeClock;
+
+    @FXML
+    void input_onbutton(ActionEvent event) {
+
+    }
+
+}
